01159e0faf825cd4bc10cb1a4da35e06a467f8e6
marc-pas <145859294+marc-pas@users.noreply.github.com>
2026-04-11 11:38:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'trainboard-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'trainboard-v3';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
